b60e8118c47e9f20ced4f10130afae29cec62599
hcsarker <hcsarker2002@gmail.com>
2025-10-24 14:20:39 +0600

    vite and vercel config updated

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/", // ✅ Add this line
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react()].filter(Boolean),
+  plugins: [react()],
   build: {
     rollupOptions: {
       output: {
